462aece203dd64a045cb9aaedfd799bdb53397cd
Mario Rabelo <mutrano@gmail.com>
2021-02-18 17:47:29 -0300

    created user entity and started workin on vaccination entity

---
+++ src/main/java/com/mutra/zupchallenge/domain/User.java
@@ -0,0 +1,93 @@
+package com.mutra.zupchallenge.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity(name = "tb_user")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String name;
+	
+	@Column(unique = true)
+	private String email;
+	
+	@Column(unique = true)
+	private String cpf;
+	
+	private String dateOfBirth;
+	
+	public User() {
+		
+	}
+
+	public User(Integer id, String name, String email, String cpf, String dateOfBirth) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.cpf = cpf;
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+}
---
+++ src/main/java/com/mutra/zupchallenge/domain/Vaccination.java
@@ -0,0 +1,10 @@
+package com.mutra.zupchallenge.domain;
+
+import javax.persistence.Entity;
+
+@Entity(name = "tb_vaccination")
+public class Vaccination {
+
+	private Integer id;
+	
+}
